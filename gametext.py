e30e35b0910d301f9a0b9b7fe317e0d6ed48a348
EimanDev <e.babeckas@protonmail.com>
2026-05-20 22:19:42 +0200

    Changes to add scorebord

---
+++ gametext.py
@@ -0,0 +1,5 @@
+import pygame
+
+
+class Gametext():
+    def __init__(self):
--- main.py
+++ main.py
@@ -10,21 +10,27 @@ import sys
 
 
 def main():
-    pygame.init()
+    pygame.init()  # Initialize the game
+
+    # Here are all the sprite groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+
+    # Containers
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
     Shot.containers = (updatable, drawable, shots)
+
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+    score = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
     while True:
@@ -46,6 +52,7 @@ def main():
                     log_event("asteroid_shot")
                     bullet.kill()
                     ast.split()
+                    score += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
